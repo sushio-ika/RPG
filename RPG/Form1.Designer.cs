f9b68be83260a7d446fb7623918a0c8d16d13af3
正田 陸 <kd1340354@kobedenshi078.onmicrosoft.com>
2025-07-14 12:11:25 +0900

    プロジェクト ファイルを追加します。

---
+++ RPG/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace RPG
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("ＭＳ 明朝", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(143, 313);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(514, 78);
+            this.label1.TabIndex = 0;
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(310, 120);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(167, 73);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ RPG/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RPG
+{
+    public partial class Form1 : Form
+    {
+        private string outchar= "やあ！　こんにちは！" ;
+        private int i;
+        private int SPACE_INTERVAL = 500; // スペースの間隔（ミリ秒）
+        private int NORMAL_INTERVAL = 100; // 通常の間隔（ミリ秒）
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            i = 0;
+            timer1.Enabled = true;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (i < outchar.Length)
+            {
+                if (char.IsWhiteSpace(outchar[i])) // または currentChar == ' ' でも可
+                {
+                    timer1.Interval = SPACE_INTERVAL; // 一時的に間隔を長くする
+                }
+                else
+                {
+                    timer1.Interval = NORMAL_INTERVAL; // 通常の間隔に戻す
+                }
+                // 1文字ずつ追加して表示
+                label1.Text += outchar[i];
+                System.Media.SoundPlayer player= new System.Media.SoundPlayer("se_select_003.wav");
+                player.Play();
+                i++;
+            }
+            else
+            {
+                // 全ての文字が表示されたらタイマーを停止
+                timer1.Stop();
+            }
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            i = 0; // インデックスをリセット
+            label1.Text = ""; // ラベルをクリア
+
+            // 表示する文字列が空でなければタイマーを開始
+            if (!string.IsNullOrEmpty(outchar))
+            {
+                timer1.Start();
+            }
+        }
+    }
+}
